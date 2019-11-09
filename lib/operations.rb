c8185acc4bccede09ef06673536eda7b530b42e1
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-09 23:27:49 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
-def unsafe?(speed)
-
+def unsafe?(speed = 65)
+  speed = 60 
+  if speed > 60
+    return true 
+  elsif speed == 40 
+    return false 
+end
+unsafe?(70)
 end
 
 
 
-def not_safe?(speed)
-	
+
+def not_safe?
+speed = 65
+speed < 60 ? "safe" : "not safe"
 end
 	
 
